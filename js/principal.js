60cf981ee29957c84d570bf34fb1223826d74e10
Vieira <suellyn.s.vieira@accenture.com>
2017-11-06 16:21:29 -0200

    Variáveis e Operadores

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,36 @@
-console.log("Fui carregado de um arquivo externo");
-
+//Alterando conteúdo do h1
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+//Calculando  IMC
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+//Validando se o peso e altura sao validas
+var pesoValido = true;
+var alturaValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoValido = false;
+    tdImc.textContent = "Peso inválido";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+if (alturaValida && pesoValido) {
+    var imc = peso / (altura * altura); // 100 / (2.0 * 2.0) = 25
+    tdImc.textContent = imc;    
+}
+
